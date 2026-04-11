39d10f9185d81e02182440ff3f51b988284f82b2
Scott Gibb <smgibb@yahoo.com>
2026-04-11 21:30:04 +0100

    fix: style issues arround and fmt issues
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- examples/read_pots.rs
+++ examples/read_pots.rs
@@ -1,6 +1,13 @@
 #![no_std]
 #![no_main]
 
+use cortex_m_rt::entry;
+use panic_probe as _;
+
+
+#[entry]
 fn main() -> ! {
-    loop {}
+    loop {
+        panic!("This is a panic message!");
+    }
 }
