024612d0ff4b5c2b8f14dcb05053e7693b10f3bb
Scott Gibb <smgibb@yahoo.com>
2026-04-12 19:48:54 +0100

    feat: add working ADC Example
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- examples/read_pots.rs
+++ examples/read_pots.rs
@@ -1,12 +1,29 @@
 #![no_std]
 #![no_main]
 
+use cortex_m::prelude::_embedded_hal_adc_OneShot;
 use cortex_m_rt::entry;
+use defmt::info;
+use firmware::{log_metadata, setup_device};
 use panic_probe as _;
 
 #[entry]
 fn main() -> ! {
+    let mut device = setup_device();
+    log_metadata();
+
     loop {
-        panic!("This is a panic message!");
+        info!("Reading ADC values");
+        let channel_0_value: u16 = device
+            .adc
+            .read(&mut device.channel_0_adc)
+            .expect("This should not fail");
+        let channel_1_value: u16 = device
+            .adc
+            .read(&mut device.channel_1_adc)
+            .expect("This should not fail");
+        info!("Channel 0 ADC value: {}", channel_0_value);
+        info!("Channel 1 ADC value: {}", channel_1_value);
+        cortex_m::asm::delay(8_000_000);
     }
 }
