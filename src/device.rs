d1ccdba0f7505fd098b19929c7f4d06347d15185
Scott Gibb <smgibb@yahoo.com>
2026-04-12 19:39:25 +0100

    refactor: add device struct
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/device.rs
+++ src/device.rs
@@ -1,4 +1,19 @@
+use stm32f1xx_hal::gpio;
+use stm32f1xx_hal::gpio::gpioa;
+use stm32f1xx_hal::gpio::gpiob;
+use stm32f1xx_hal::gpio::gpioc;
 pub struct Device {
-    // pub channel_one : led_driver,
-    // pub channel_two : led_driver,
+    pub onboard_led: gpioc::PC13<gpio::Output<gpio::PushPull>>,
+
+    // pwm channels
+    pub channel_0_pwm: gpiob::PB0<gpio::Alternate<gpio::PushPull>>,
+    pub channel_1_pwm: gpiob::PB1<gpio::Alternate<gpio::PushPull>>,
+
+    // led pwm channels
+    pub channel_0_led_pwm: gpioa::PA6<gpio::Alternate<gpio::PushPull>>,
+    pub channel_1_led_pwm: gpioa::PA7<gpio::Alternate<gpio::PushPull>>,
+
+    // ADC channels
+    pub channel_0_adc: gpioa::PA0<gpio::Analog>,
+    pub channel_1_adc: gpioa::PA1<gpio::Analog>,
 }
