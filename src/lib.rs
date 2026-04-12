f380c2d0d3d483827ba41402d589d99cf8366a8c
Scott Gibb <smgibb@yahoo.com>
2026-04-12 19:39:25 +0100

    refactor: add device struct
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,55 @@
 #![no_std]
+
+use crate::device::Device;
+use defmt::info;
+use defmt_rtt as _;
+use panic_probe as _;
+use stm32f1xx_hal::pac;
+use stm32f1xx_hal::prelude::*;
 mod device;
+commitment_issues::include_metadata!();
+
+pub fn log_metadata() {
+    info!("Train Driver Metadata:");
+    info!("Schema version:  {}", metadata::schema());
+    info!("Compile time:    {}", metadata::compile_time());
+    info!("Commit hash:     {}", metadata::short_hash());
+    info!("Is dirty build:  {}", metadata::is_dirty());
+    info!("Tag description: {}", metadata::tag_describe());
+    info!("Last author:     {}", metadata::last_author());
+}
+
+pub fn setup_device() -> Device {
+    // Get access to the device specific peripherals from the peripheral access crate
+    let dp = pac::Peripherals::take().unwrap();
+    let mut rcc = dp.RCC.constrain();
+
+    let mut gpioa = dp.GPIOA.split(&mut rcc);
+    let mut gpiob = dp.GPIOB.split(&mut rcc);
+    let mut gpioc = dp.GPIOC.split(&mut rcc);
+
+    let onboard_led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+
+    // pwm channels
+    let channel_0_pwm = gpiob.pb0.into_alternate_push_pull(&mut gpiob.crl);
+    let channel_1_pwm = gpiob.pb1.into_alternate_push_pull(&mut gpiob.crl);
+
+    // led pwm channels
+    let channel_0_led_pwm = gpioa.pa6.into_alternate_push_pull(&mut gpioa.crl);
+    let channel_1_led_pwm = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+
+    // ADC channels
+
+    let channel_0_adc = gpioa.pa0.into_analog(&mut gpioa.crl);
+    let channel_1_adc = gpioa.pa1.into_analog(&mut gpioa.crl);
+
+    Device {
+        onboard_led,
+        channel_0_pwm,
+        channel_1_pwm,
+        channel_0_led_pwm,
+        channel_1_led_pwm,
+        channel_0_adc,
+        channel_1_adc,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,57 +5,22 @@
 //! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
 //! the reference manual for an explanation. This is not an issue on the blue pill.
 
-#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
-commitment_issues::include_metadata!();
-
-use defmt::info;
-use nb::block;
-
-use defmt_rtt as _;
-use panic_probe as _;
 
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
+use firmware::{log_metadata, setup_device};
 
 #[entry]
 fn main() -> ! {
     // Get access to the core peripherals from the cortex-m crate
-    let cp = cortex_m::Peripherals::take().unwrap();
-    // Get access to the device specific peripherals from the peripheral access crate
-    let dp = pac::Peripherals::take().unwrap();
+    let mut device = setup_device();
+    log_metadata();
 
-    let mut rcc = dp.RCC.constrain();
-
-    // Acquire the GPIOC peripheral
-    let mut gpioc = dp.GPIOC.split(&mut rcc);
-
-    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
-    // in order to configure the port. For pins 0-7, crl should be passed instead.
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cp.SYST, &rcc.clocks).counter_hz();
-    timer.start(1.Hz()).unwrap();
-
-    // Wait for the timer to trigger an update and change the state of the LED
     loop {
-        metadata();
-        block!(timer.wait()).unwrap();
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        led.set_low();
+        device.onboard_led.set_low();
+        cortex_m::asm::delay(8_000_000);
+        device.onboard_led.set_high();
+        cortex_m::asm::delay(8_000_000);
     }
-}
-
-fn metadata() {
-    info!("Hello, world!");
-
-    info!("\n Here is the binary's metadata:\n");
-    info!("Schema version:  {}", metadata::schema());
-    info!("Compile time:    {}", metadata::compile_time());
-    info!("Commit hash:     {}", metadata::short_hash());
-    info!("Is dirty build:  {}", metadata::is_dirty());
-    info!("Tag description: {}", metadata::tag_describe());
-    info!("Last author:     {}", metadata::last_author());
 }
