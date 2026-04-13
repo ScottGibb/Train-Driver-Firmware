03547aa5fd795aa50cd7cc9a93d5a4c8cf9a9f23
Scott Gibb <smgibb@yahoo.com>
2026-04-13 21:29:01 +0100

    feat: Add PWM Driver
    MVP driving circuit with hardware, still to be tested on actual trains
    
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

---
+++ src/pwm_driver.rs
@@ -0,0 +1,31 @@
+use stm32f1xx_hal::{
+    pac::TIM2,
+    timer::{C3, C4, PwmChannel},
+};
+
+use crate::{
+    device::{ChannelOnePwm, ChannelZeroPwm},
+    types::Percentage,
+};
+
+pub struct PwmDriver {
+    pub channel_0_pwm: ChannelZeroPwm,
+    pub channel_1_pwm: ChannelOnePwm,
+}
+
+impl PwmDriver {
+    pub fn set_channel_0_duty(&mut self, duty: Percentage) {
+        let value = u16::from(duty);
+
+        let max_value = self.channel_0_pwm.get_max_duty();
+        let scaled_value = (value as u32 * max_value as u32 / 100) as u16;
+        self.channel_0_pwm.set_duty(scaled_value);
+    }
+
+    pub fn set_channel_1_duty(&mut self, duty: Percentage) {
+        let value = u16::from(duty);
+        let max_value = self.channel_1_pwm.get_max_duty();
+        let scaled_value = (value as u32 * max_value as u32 / 100) as u16;
+        self.channel_1_pwm.set_duty(scaled_value);
+    }
+}
