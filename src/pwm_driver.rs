f39d053506e134ec50bf6252d1f926a03b1b5831
Scott Gibb <smgibb@yahoo.com>
2026-04-13 21:53:03 +0100

    refactor: percentage type
    removed try from and added scale_to method using a trick for percision
    in MCU division
    
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/pwm_driver.rs
+++ src/pwm_driver.rs
@@ -10,17 +10,12 @@ pub struct PwmDriver {
 
 impl PwmDriver {
     pub fn set_channel_0_duty(&mut self, duty: Percentage) {
-        let value = u16::from(duty);
-
-        let max_value = self.channel_0_pwm.get_max_duty();
-        let scaled_value = (value as u32 * max_value as u32 / 100) as u16;
+        let scaled_value = duty.scale_to(self.channel_0_pwm.get_max_duty());
         self.channel_0_pwm.set_duty(scaled_value);
     }
 
     pub fn set_channel_1_duty(&mut self, duty: Percentage) {
-        let value = u16::from(duty);
-        let max_value = self.channel_1_pwm.get_max_duty();
-        let scaled_value = (value as u32 * max_value as u32 / 100) as u16;
+        let scaled_value = duty.scale_to(self.channel_1_pwm.get_max_duty());
         self.channel_1_pwm.set_duty(scaled_value);
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -3,47 +3,29 @@ use defmt::Format;
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Format)]
 pub struct Percentage(u8);
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Format)]
 pub enum PercentageError {
     InvalidValue,
 }
 
 impl Percentage {
+    pub const MAX: u8 = 100;
+
     pub fn new(value: u8) -> Result<Self, PercentageError> {
-        if value > 100 {
-            return Err(PercentageError::InvalidValue);
+        if value <= Self::MAX {
+            Ok(Self(value))
+        } else {
+            Err(PercentageError::InvalidValue)
         }
-        Ok(Self(value))
     }
-}
 
-impl From<Percentage> for u8 {
-    fn from(value: Percentage) -> Self {
-        value.0
+    pub fn get(self) -> u8 {
+        self.0
     }
-}
 
-impl TryFrom<u8> for Percentage {
-    type Error = PercentageError;
-
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        Percentage::new(value)
-    }
-}
-
-impl From<Percentage> for u16 {
-    fn from(value: Percentage) -> Self {
-        value.0 as u16
-    }
-}
-
-impl TryFrom<u16> for Percentage {
-    type Error = PercentageError;
-
-    fn try_from(value: u16) -> Result<Self, Self::Error> {
-        if value > 100 {
-            return Err(PercentageError::InvalidValue);
-        }
-        Ok(Self(value as u8))
+    pub fn scale_to(self, max: u16) -> u16 {
+        let value = self.0 as u32;
+        let max = max as u32;
+        ((value * max + (Self::MAX as u32 / 2)) / Self::MAX as u32) as u16
     }
 }
