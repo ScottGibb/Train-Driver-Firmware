f7f79970ab8b53f6b96685a44593817fb0a9ef32
Scott Gibb <smgibb@yahoo.com>
2026-04-13 22:29:26 +0100

    feat: refactor and implement generic PWM Driver
    A simple struct that contains a array of pwm signals. It keeps track of
    their duties and handles the embedded-hal API.
    
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/pwm_driver.rs
+++ src/pwm_driver.rs
@@ -1,21 +1,65 @@
-use crate::{
-    device::{ChannelOnePwm, ChannelZeroPwm},
-    types::Percentage,
-};
+use crate::types::Percentage;
+use core::convert::Infallible;
+use embedded_hal::pwm::SetDutyCycle;
 
-pub struct PwmDriver {
-    pub channel_0_pwm: ChannelZeroPwm,
-    pub channel_1_pwm: ChannelOnePwm,
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum PwmDriverError {
+    InvalidChannel,
 }
 
-impl PwmDriver {
-    pub fn set_channel_0_duty(&mut self, duty: Percentage) {
-        let scaled_value = duty.scale_to(self.channel_0_pwm.get_max_duty());
-        self.channel_0_pwm.set_duty(scaled_value);
+/// A generic PWM driver that can control multiple PWM channels using a common interface.
+/// Uses a Percentage type for duty cycle values, which can be easily converted to the appropriate duty cycle values for each channel.
+pub struct PwmDriver<'a, const NUM_CHANNELS: usize> {
+    channels: [&'a mut dyn SetDutyCycle<Error = Infallible>; NUM_CHANNELS],
+    channel_duties: [Percentage; NUM_CHANNELS],
+}
+
+impl<'a, const NUM_CHANNELS: usize> PwmDriver<'a, NUM_CHANNELS> {
+    pub fn new(
+        // The lifetime of the channel references must be the same as the lifetime of the PwmDriver struct,
+        // since the channels are stored as references inside the struct. This ensures that the channels will
+        // not be dropped while the PwmDriver is still using them.
+        // Using a vtable with the dyn keyword to allow me to create a single PwmDriver struct that can
+        // work with any number of channels, without needing to create separate structs for each possible number of channels.
+        // This also allows for more flexible and dynamic usage of the PwmDriver, as the channels can be provided at
+        // runtime rather than being fixed at compile time.
+        channels: [&'a mut dyn SetDutyCycle<Error = Infallible>; NUM_CHANNELS],
+    ) -> Self {
+        Self {
+            channels,
+            channel_duties: [Percentage::new(0).expect("This should not fail"); NUM_CHANNELS],
+        }
     }
 
-    pub fn set_channel_1_duty(&mut self, duty: Percentage) {
-        let scaled_value = duty.scale_to(self.channel_1_pwm.get_max_duty());
-        self.channel_1_pwm.set_duty(scaled_value);
+    pub fn set_duty(
+        &mut self,
+        channel_index: usize,
+        duty: Percentage,
+    ) -> Result<(), PwmDriverError> {
+        let channel = self
+            .channels
+            .get_mut(channel_index)
+            .ok_or(PwmDriverError::InvalidChannel)?;
+        channel
+            .set_duty_cycle_percent(duty.get())
+            .expect("PWM is infallible");
+        self.channel_duties[channel_index] = duty;
+        Ok(())
+    }
+
+    pub fn get_duty(&self, channel_index: usize) -> Result<Percentage, PwmDriverError> {
+        self.channel_duties
+            .get(channel_index)
+            .copied()
+            .ok_or(PwmDriverError::InvalidChannel)
+    }
+
+    pub fn turn_all_off(&mut self) {
+        for (i, channel) in self.channels.iter_mut().enumerate() {
+            channel
+                .set_duty_cycle_percent(0)
+                .expect("PWM is infallible");
+            self.channel_duties[i] = Percentage::new(0).expect("This should not fail");
+        }
     }
 }
