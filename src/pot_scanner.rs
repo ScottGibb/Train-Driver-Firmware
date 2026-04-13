077ae50160dfb1cf7c4e025bdd7bb79c37c3aa22
Scott Gibb <smgibb@yahoo.com>
2026-04-13 23:11:43 +0100

    feat: generified pot_scanner
    Attempt at generifying pot scanner to use embedded_hal traits for ADC.
    There is none, so tried to use stm32 hals, but it does not work as well
    as I would hope.
    
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/pot_scanner.rs
+++ src/pot_scanner.rs
@@ -1,58 +1,49 @@
-use cortex_m::prelude::_embedded_hal_adc_OneShot;
-use stm32f1xx_hal::{adc::Adc, pac::ADC1};
+use crate::device::DeviceError;
+use crate::types::Percentage;
+use stm32f1xx_hal::adc::Adc;
+use stm32f1xx_hal::adc::Instance;
+use stm32f1xx_hal::hal_02::adc::Channel;
+use stm32f1xx_hal::hal_02::adc::OneShot;
 
-use crate::{
-    device::{ChannelOneAdc, ChannelZeroAdc, DeviceError},
-    types::Percentage,
-};
-
-//TODO: Replace this code with generic code that can be used with any ADC and any pins, instead of hardcoding the specific pins and ADC instance. This will make it more reusable and adaptable to different hardware configurations.
-pub const LOW_THRESHOLD: u16 = 0;
-pub const HIGH_THRESHOLD: u16 = 4095;
-pub struct PotScanner {
-    adc: Adc<ADC1>,
-    adc_channel_0: ChannelZeroAdc,
-    adc_channel_1: ChannelOneAdc,
+pub struct PotScanner<ADC: Instance, P0, P1> {
+    adc: Adc<ADC>,
+    channel_0: P0,
+    channel_1: P1,
 }
 
-impl PotScanner {
-    pub fn new(
-        adc: Adc<ADC1>,
-        adc_channel_0: ChannelZeroAdc,
-        adc_channel_1: ChannelOneAdc,
-    ) -> Self {
-        PotScanner {
+impl<ADC, P0, P1> PotScanner<ADC, P0, P1>
+where
+    ADC: Instance,
+    P0: Channel<ADC, ID = u8>,
+    P1: Channel<ADC, ID = u8>,
+    Adc<ADC>: OneShot<ADC, u16, P0, Error = ()> + OneShot<ADC, u16, P1, Error = ()>,
+{
+    pub const LOW_THRESHOLD: u16 = 0;
+    pub const HIGH_THRESHOLD: u16 = 4095;
+
+    pub fn new(adc: Adc<ADC>, channel_0: P0, channel_1: P1) -> Self {
+        Self {
             adc,
-            adc_channel_0,
-            adc_channel_1,
+            channel_0,
+            channel_1,
         }
     }
 
     pub fn scan(&mut self) -> Result<(Percentage, Percentage), DeviceError> {
-        let mut channel_0_value: u16 = self
+        let raw_0: u16 = self
             .adc
-            .read(&mut self.adc_channel_0)
+            .read(&mut self.channel_0)
             .map_err(|_| DeviceError::AdcError)?;
-        let mut channel_1_value: u16 = self
+        let raw_1: u16 = self
             .adc
-            .read(&mut self.adc_channel_1)
+            .read(&mut self.channel_1)
             .map_err(|_| DeviceError::AdcError)?;
 
-        // Clamp inside the valid range to avoid issues with out-of-range values
-        channel_0_value = channel_0_value.clamp(LOW_THRESHOLD, HIGH_THRESHOLD);
-        channel_1_value = channel_1_value.clamp(LOW_THRESHOLD, HIGH_THRESHOLD);
-
-        // Calculate the percentage values
-        channel_0_value = ((channel_0_value - LOW_THRESHOLD) as u32 * 100
-            / (HIGH_THRESHOLD - LOW_THRESHOLD) as u32) as u16;
-        channel_1_value = ((channel_1_value - LOW_THRESHOLD) as u32 * 100
-            / (HIGH_THRESHOLD - LOW_THRESHOLD) as u32) as u16;
-
-        let channel_0_percentage =
-            Percentage::new(channel_0_value as u8).map_err(|_| DeviceError::ConversionError)?;
-        let channel_1_percentage =
-            Percentage::new(channel_1_value as u8).map_err(|_| DeviceError::ConversionError)?;
-
-        Ok((channel_0_percentage, channel_1_percentage))
+        Ok((
+            Percentage::to_percentage(raw_0, Self::HIGH_THRESHOLD, Self::LOW_THRESHOLD)
+                .map_err(|_| DeviceError::ConversionError)?,
+            Percentage::to_percentage(raw_1, Self::HIGH_THRESHOLD, Self::LOW_THRESHOLD)
+                .map_err(|_| DeviceError::ConversionError)?,
+        ))
     }
 }
